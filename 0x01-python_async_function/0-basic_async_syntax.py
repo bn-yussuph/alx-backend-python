1f9d96b5e941f2772cbf5b6b81dcbda19c318be0
bn-yussuph <sulehajjaajj@gmail.com>
2025-04-29 01:43:55 +0000

    fix indentation problem of task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
+"""
+Function waits for a random delay between 0 and `max_delay` (included and float
+value) seconds and eventually returns it.
+"""
 
 import asyncio
 from random import random
 
 async def wait_random(max_delay: int = 10):
+    """
+    Parameters
+        max_delay: int, default 10
+
+    Return
+        float(value): random delay between 0 and `max_delay` seconds
+    """
     wait = max_delay * random()
     await asyncio.sleep(wait)
     return wait
